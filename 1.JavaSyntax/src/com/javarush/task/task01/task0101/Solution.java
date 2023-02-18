9a987f209ab44d5a6caec4542b9e36916792d868
romanisaev <dr.paperr@mail.ru>
2023-02-17 23:02:52 -0800

    add solution javaSyntax task 0101

--- 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task01.task0101;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println("Я думаю, быть программистом круто");
     }
 }
