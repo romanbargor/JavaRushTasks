c4fd541f4bb3c1c3031f9216e2a8363be8d73e60
romanisaev <dr.paperr@mail.ru>
2023-02-19 00:22:44 -0800

    add solution javaSyntax task 0101

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 31;
+        int b = 28;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
