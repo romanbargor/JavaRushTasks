165fcf39c363d44bfeb41be160f4bb900a6c30bf
romanisaev <dr.paperr@mail.ru>
2023-02-19 00:47:48 -0800

    add solution javaSyntax task 0101

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0104/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task01.task0104;
+
+/* 
+Починить систему уборки города
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Уборка города началась");
+       /* System.out.println("убираем людей"); //людей убирать не нужно
+        System.out.println("убираем животных")*/; //животных убирать не нужно
+        System.out.println("убираем мусор");
+        System.out.println("убираем листья");
+        /*System.out.println("убираем птиц")*/; //птиц убирать не нужно
+        System.out.println("уборка города закончилась");
+    }
+}
