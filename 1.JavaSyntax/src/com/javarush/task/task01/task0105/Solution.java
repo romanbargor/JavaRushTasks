1a3f519df2efa6c2c69c5f64bd77708ae38934fa
romanisaev <dr.paperr@mail.ru>
2023-02-18 22:19:26 -0800

    add solution javaSyntax task 0101

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task01.task0105;
+
+/* 
+
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String name = "Roman";
+        int age = 24;
+        String city = "Saint-Petersburg";
+        System.out.println (name);
+        System.out.println (age);
+        System.out.println (city);
+
+
+
+
+
+
+
+    }
+}
