015deb74479b558571e582725faf967c583349aa
romanisaev <dr.paperr@mail.ru>
2023-03-10 22:51:08 -0800

    add solution javaSyntax task 0103(2)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 4;
+        int age = 5;
+        String documents = "имеются";
+        String behavior = "отсутствуют";
+
+        System.out.println(id);
+        System.out.println(age);
+        System.out.println(documents);
+        System.out.println(behavior);
+
+    }
+}
