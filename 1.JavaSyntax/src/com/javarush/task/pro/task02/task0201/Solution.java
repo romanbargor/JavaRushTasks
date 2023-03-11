926c31b22faf1f1b442924b351807d75151b4a0c
romanisaev <dr.paperr@mail.ru>
2023-03-10 22:56:09 -0800

    add solution javaSyntax task 0103(2)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 3;
+        int numericValue = 5;
+        int size = 7;
+        int number = 11;
+
+        System.out.println(intValue);
+        System.out.println(numericValue);
+        System.out.println(size);
+        System.out.println(number);
+
+    }
+}
