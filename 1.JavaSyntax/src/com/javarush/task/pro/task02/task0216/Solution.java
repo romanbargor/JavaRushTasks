d476f082ee1a79b2b9d42cfaf148a10c986a01b2
romanisaev <dr.paperr@mail.ru>
2023-03-13 00:52:45 -0700

    add solution javaSyntax task 0103(2)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Roman";
+        String date = "twenty one november";
+        String time = "nine o'cloack";
+        String noun = "mom";
+        String adjective = "insurgent";
+        String verb = "add";
+
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
