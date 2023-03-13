a58478661eb2ba728610be8bfacb122165af183b
romanisaev <dr.paperr@mail.ru>
2023-03-13 00:08:12 -0700

    add solution javaSyntax task 0103(2)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "all";
+        String phrase = "many";
+        String line = "life";
+        String text = "english";
+
+        System.out.println(word + phrase + line + text);
+    }
+}
