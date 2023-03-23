b3eb1d652bcd81871c1013fdf9ea671208a90105
romanisaev <dr.paperr@mail.ru>
2023-03-22 19:43:45 -0700

    add solution javaSyntax task 0103(2)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0317/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task03.task0317;
+
+import java.util.Scanner;
+
+/* 
+Пищевой конвеер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+        int c = console.nextInt();
+        int d = console.nextInt();
+
+        if (a>0)
+            System.out.println(a);
+        if (b>0)
+            System.out.println(b);
+        if (c>0)
+            System.out.println(c);
+        if (d>0)
+            System.out.println(d);
+
+    }
+}
