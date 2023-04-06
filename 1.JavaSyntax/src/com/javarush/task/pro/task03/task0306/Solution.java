ebb4972a41e2e9768bd3e60ffcaa2d57fc3cc987
romanisaev <dr.paperr@mail.ru>
2023-04-05 20:00:25 -0700

    java rush task 0314

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0306/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task03.task0306;
+
+import java.util.Scanner;
+
+/* 
+Треугольник
+*/
+
+public class Solution {
+    private static final String TRIANGLE_EXISTS = "треугольник существует";
+    private static final String TRIANGLE_NOT_EXISTS = "треугольник не существует";
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+        int c = console.nextInt();
+
+        if (a < (c + b) && b < (a + c) && c < (b + a)) {
+            System.out.println(TRIANGLE_EXISTS);
+        } else {
+            System.out.println(TRIANGLE_NOT_EXISTS);
+
+
+        }
+    }
+}
