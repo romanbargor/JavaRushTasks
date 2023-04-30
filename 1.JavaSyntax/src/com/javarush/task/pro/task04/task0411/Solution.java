38799ee1b5a554f16cbef2cd2939f87751a216ed
romanisaev <dr.paperr@mail.ru>
2023-04-29 20:23:28 -0700

    java rush task 0411

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0411/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task04.task0411;
+
+/* 
+Четные числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        for (int a = 2; a <= 15; a+=2) {
+            System.out.println(a);
+        }
+
+    }
+
+}
