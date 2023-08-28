d7e95cd057981988a65d9c78691ff20ed4271bd5
romanisaev <dr.paperr@mail.ru>
2023-08-28 01:00:38 -0700

    java rush task 0510

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task05.task0510;
+
+/* 
+Треугольный массив
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+         result = new int[10][];
+        result[0] = new int[1];
+        result[1] = new int[2];
+        result[3] = new int[4];
+        result[4] = new int[5];
+        result[5] = new int[6];
+        result[6] = new int[7];
+        result[7] = new int[8];
+        result[8] = new int[9];
+        result[9] = new int[10];
+        for (int i = 0; i < result.length ; i++) {
+            result[i] = new int[i+1];
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = i+j;
+                System.out.print(result[i][j]+" ");
+
+
+            }System.out.println();
+
+
+
+        }
+    }
+}
