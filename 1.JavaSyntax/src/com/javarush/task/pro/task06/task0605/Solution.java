5bd235eec4b9d91c717ce9051fcee43839fc6e52
romanisaev <dr.paperr@mail.ru>
2023-09-21 11:46:25 -0700

    java rush task 0605

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task06.task0605;
+
+/* 
+Правильный порядок
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        int n = array.length;
+        int temp;
+        for (int i = 0; i < n/2; i++) {
+            temp = array[n-i-1];
+            array[n-i-1] = array[i];
+            array[i] = temp;
+
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
