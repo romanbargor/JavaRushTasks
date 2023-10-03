de5bb481926fe2c7b65dd1c582066b8d95e71928
romanisaev <dr.paperr@mail.ru>
2023-10-03 12:36:34 -0700

    java rush task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(String s, int c) {
+
+    }
+    public static void universalMethod(boolean a, boolean b) {
+
+    }
+    public static void universalMethod(String a, boolean s) {
+
+    }
+    public static void universalMethod(String a, String b) {
+
+    }
+    public static void universalMethod(char s, char c) {
+
+    }
+    public static void universalMethod(int a, int b) {
+
+    }
+    public static void universalMethod(String alm, char alt) {
+
+    }
+    public static void universalMethod(char a, boolean c) {
+
+    }
+    public static void universalMethod(char a, int b) {
+
+    }
+
+
+
+
+
+}
