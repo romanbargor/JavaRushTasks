a37a81d555478c54caeae346c8aba15f920b054d
romanisaev <dr.paperr@mail.ru>
2023-10-13 21:58:12 -0700

    java rush task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int m;
+    char s;
+    long k;
+    short a;
+    double w;
+    float u;
+    boolean h;
+    byte l;
+}
