8024274dc425f2b5fdcf5fb0c70154e015e7aee1
romanisaev <dr.paperr@mail.ru>
2023-10-13 22:21:01 -0700

    java rush task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    int p = Integer.MAX_VALUE;
+    long a = Long.MAX_VALUE;
+    short b = Short.MAX_VALUE;
+    byte c = Byte.MAX_VALUE;
+    double f = Double.MAX_VALUE;
+    float l = Float.MAX_VALUE;
+    boolean n = true;
+    char i = (char) Short.MAX_VALUE;
+
+
+
+}
