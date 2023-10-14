eea494503b6f9429ec12c3f501fb413f4e9e0589
romanisaev <dr.paperr@mail.ru>
2023-10-13 22:25:34 -0700

    java rush task 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    int a = Integer.MIN_VALUE;
+    long b = Long.MIN_VALUE;
+    short c = Short.MIN_VALUE;
+    float d = Float.MIN_VALUE;
+    double f = Double.MIN_VALUE;
+    boolean n = false;
+    byte m = Byte.MIN_VALUE;
+    char o = (char) Short.MIN_VALUE;
+
+}
