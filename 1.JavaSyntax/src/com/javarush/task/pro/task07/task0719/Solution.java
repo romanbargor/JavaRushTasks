961548bb143bf37b122e2578829096e3cceeae1d
romanisaev <dr.paperr@mail.ru>
2023-10-14 13:47:01 -0700

    java rush task 0719

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0719/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0719;
+
+/* 
+Космическая Робосутра
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+         Gun item = new Gun();
+         AutomaticKitchen  kitchen = new AutomaticKitchen();
+         ProtectiveFieldGenerator generation = new ProtectiveFieldGenerator();
+         RepairDrone drone = new RepairDrone();
+         RobotCleaner robot = new RobotCleaner();
+
+    }
+}
