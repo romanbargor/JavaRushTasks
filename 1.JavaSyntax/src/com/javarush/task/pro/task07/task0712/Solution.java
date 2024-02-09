24c8d8c2671f2c33085eac70c8015123e02be952
romanisaev <dr.paperr@mail.ru>
2024-02-09 09:55:42 -0800

    java rush task 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = new String();
+        String b = new String ();
+        String c = new String();
+        String d = new String();
+        String i = new String();
+        String f = new String();
+        String g = new String();
+        String h = new String();
+        String j = new String();
+        String k = new String();
+
+        int[] l = new int[10];
+        int[] m = new int[10];
+        int[] o = new int[10];
+        int[] p = new int[10];
+        int[] s = new int[10];
+
+        Scanner console = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+    }
+}
